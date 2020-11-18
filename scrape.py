6b62e203117e71e6a2b5da5d94eb08c6b35072d6
Bill Davis <pabdavis@gmail.com>
2020-11-17 20:33:27 -0500

    Added selecting the 2nd sheet for downloading in the CountyCases

--- scrape.py
+++ scrape.py
@@ -2,35 +2,34 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Chrome('./chromedriver')
 
-driver.get("https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/DailyCasesandDeathsMetrics?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=yes&:animate_transition=yes&:display_static_image=no&:display_spinner=yes&:display_overlay=no&:display_count=yes&:language=en&publish=yes&:loadOrderID=0")
+class NCDHHSScraper:
+    def __init__(self, baseUrl, steps):
+        # these lines will help if someone faces issues like
+        # chrome closes after execution
+        self.opts = webdriver.ChromeOptions()
+        self.opts.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(options=self.opts)
 
-driver.implicitly_wait(3) # seconds
-tab_select = driver.find_element_by_xpath("//*[@id=\"tableauTabbedNavigation_tab_1\"]")
-tab_select.click()
+        self.driver.get(baseUrl)
+        self.driver.implicitly_wait(5)  # seconds
 
-time.sleep(2)
-dl_button = driver.find_element_by_xpath("/html/body/div[2]/div[3]/div[2]/div[1]/div[2]/div[5]/span[1]")
+        for step in steps:
+            self.driver.find_element_by_xpath(step).click()
+            if step == steps[-1]:
+                time.sleep(5)  # assumes download and forces longer sleep
 
-dl_button.click()
+        self.driver.close()
 
-time.sleep(1)
 
-cross_tab = driver.find_element_by_xpath("//*[@id=\"DownloadDialog-Dialog-Body-Id\"]/div/button[3]")
-
-cross_tab.click()
-
-time.sleep(1)
-
-get_csv = driver.find_element_by_xpath("//*[@id=\"export-crosstab-options-dialog-Dialog-BodyWrapper-Dialog-Body-Id\"]/div/div[2]/div[2]/div/label[2]")
-get_csv.click()
-
-time.sleep(1)
-dl_button = driver.find_element_by_xpath("//*[@id=\"export-crosstab-options-dialog-Dialog-BodyWrapper-Dialog-Body-Id\"]/div/div[3]/button")
-dl_button.click()
-
-time.sleep(4)
-
-driver.close()
-
+NCDHHSScraper(
+    "https://public.tableau.com/views/NCDHHS_COVID-19_DataDownload/CountyCasesandDeaths?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=yes&:toolbar=yes&:animate_transition=yes&:display_static_image=no&:display_spinner=yes&:display_overlay=no&:display_count=yes&:language=en&publish=yes&:loadOrderID=0",
+    [
+        '//*[@id="tableauTabbedNavigation_tab_2"]',  # tab select
+        "/html/body/div[2]/div[3]/div[2]/div[1]/div[2]/div[5]/span[1]",  # download button
+        '//*[@id="DownloadDialog-Dialog-Body-Id"]/div/button[3]',  # select crosstab
+        '//*[@id="export-crosstab-options-dialog-Dialog-BodyWrapper-Dialog-Body-Id"]/div/div[1]/div[2]/div/div/div[2]/div',  # select the sheet
+        '//*[@id="export-crosstab-options-dialog-Dialog-BodyWrapper-Dialog-Body-Id"]/div/div[2]/div[2]/div/label[2]',  # select csv
+        '//*[@id="export-crosstab-options-dialog-Dialog-BodyWrapper-Dialog-Body-Id"]/div/div[3]/button',  # select download button
+    ],
+)
